661ccb87142ab50e119a1b4d5bc43b5bbd5697b9
Aditya Patil <adpa@example.com>
2020-08-17 16:47:56 +0530

    made changes in python code to get varible values

--- roles/aws_check_snapshot_status/files/ses.py
+++ roles/aws_check_snapshot_status/files/ses.py
@@ -1,26 +1,31 @@
 import boto3
 from botocore.exceptions import ClientError
 
-#The deatils of the untagged snapshot
-#DETAILS = 
 
+#DEFINE ARGUMENTS  
 
-# Replace sender@example.com with your "From" address.
+# 1) Replace sender@example.com with your "From" address.
 # This address must be verified with Amazon SES.
-SENDER = "{{ sender_identity }}"
+#SENDER =  "sender_identity"
 #"patiladi3110@gmail.com" 
-# Replace recipient@example.com with a "To" address. If your account 
+sender = sys.argv[1]
+
+# 2) Replace recipient@example.com with a "To" address. If your account 
 # is still in the sandbox, this address must be verified.
-RECIPIENT = "{{ reciever_identity }}"
+#RECIPIENT = "reciever_identity"
 #"gcptesting4534@gmail.com" #
+reciever = sys.argv[2]
+
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the 
 # ConfigurationSetName=CONFIGURATION_SET argument below.
 #CONFIGURATION_SET = "ConfigSet"
 
-# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-AWS_REGION = "{{ region }}"
+# 3) If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+#AWS_REGION =  "region" 
 #"us-east-2" #
+region = sys.argv[3]
+
 # The subject line for the email.
 SUBJECT = "Warning for Untagged Snapshots"
 
@@ -44,7 +49,7 @@ BODY_HTML = """<html>
 CHARSET = "UTF-8"
 
 # Create a new SES resource and specify a region.
-client = boto3.client('ses',region_name=AWS_REGION)
+client = boto3.client('ses',region_name=region)
 
 # Try to send the email.
 try:
@@ -52,7 +57,7 @@ try:
     response = client.send_email(
         Destination={
             'ToAddresses': [
-                RECIPIENT,
+                reciever,
             ],
         },
         Message={
@@ -71,7 +76,7 @@ try:
                 'Data': SUBJECT,
             },
         },
-        Source=SENDER,
+        Source=sender,
         # If you are not using a configuration set, comment or delete the
         # following line
         #ConfigurationSetName=CONFIGURATION_SET,
