a82601ddf85173d851864775ebdf87a5a2e96efd
Aditya Patil <adpa@example.com>
2020-08-26 14:54:37 +0530

    created Report_json_to_csv.py to convert json report to csv

---
+++ roles/azure_get_untagged_snapshots/files/Report_json_to_csv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import json
+import csv
+import sys
+import os
+
+#raw_file = sys.argv[1]
+#actual_file = sys.argv[2]
+
+header = ['Name', 'ResourceGroup', 'StartTime', 'SKU Tier', 'SnapshotID']
+
+#if you are not using utf-8 files, remove the next line
+sys.setdefaultencoding("UTF-8") #set the encode to utf8
+#check if you pass the input file and output file
+
+if sys.argv[1] is not None and sys.argv[2] is not None:
+    raw_file = sys.argv[1]
+    actual_file = sys.argv[2]
+    
+    inputFile = open(raw_file) #open json file
+    outputFile = open(actual_file, 'w') #load csv file
+    
+    data = json.load(inputFile) #load json content
+    inputFile.close() #close the input file
+    
+    output = csv.writer(outputFile) #create a csv.write
+    output.writerow(header)  # header row
+    #output.writerow(data[0].keys())  # header row
+    for row in data:
+        output.writerow(row.values()) #values row
