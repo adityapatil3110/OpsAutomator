e915493d4fdee2f31ff4e13e9ba63f876e89d4ce
Aditya Patil <adpa@example.com>
2020-09-22 01:46:17 +0530

    test

--- roles/azure_delete_older_snapshots/files/calculateDays.py
+++ roles/azure_delete_older_snapshots/files/calculateDays.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
 import os
+import datetime
 import sys
 from datetime import datetime
 import pandas as pd
 
-snapshot_date_obj = sys.argv[1]
+snapshot_date_raw_str = sys.argv[1]
+snapshot_date_raw_str = snapshot_date_raw_str[:-6]
+snapshot_date_raw_str = snapshot_date_raw_str.replace("T", " ")
+snapshot_date = datetime.strptime(snapshot_date_raw_str, '%Y-%m-%d %H:%M:%S.%f')
 
-snapshot_date = datetime.strptime(snapshot_date_obj, '%Y-%m-%d%H:%M:%S.%f%:z')
-print (snapshot_date)
+#print(snapshot_date)
+
 
 ######## Function for days_old
-
 def days_old(date):
-    date_obj = datetime.datetime(date)
+    date_obj = date.replace(tzinfo=None)
     diff = datetime.now() - date_obj
     return diff.days
 
 
-days = days_old(snapshot_date)
+snapshot_age = int (days_old(snapshot_date))
+print(snapshot_age)
