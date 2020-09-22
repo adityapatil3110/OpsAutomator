44f2ab0bd0f2d2a7a20a4c6f8b52c5c1124dfd95
Aditya Patil <adpa@example.com>
2020-09-22 19:27:25 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -9,5 +9,5 @@ import pandas as pd
 snapshot_response = sys.argv[1]
 
 for snapshot in snapshot_response:
-    snapshot_name = snapshot['name']
+    snapshot_name = snapshot[0]['name']
     print(snapshot_name)
