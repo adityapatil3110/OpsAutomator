8b87f80a999849043727d1e99e49b4811062d6fd
Aditya Patil <adpa@example.com>
2020-09-23 00:59:13 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -3,12 +3,15 @@
 import os
 import datetime
 import sys
+import json
 from datetime import datetime
 import pandas as pd
 
 snapshot_response = sys.argv[1]
-#print (snapshot_response)
+snapshot_response = json.dumps(snapshot_response)
+snapshot_response = json.loads(snapshot_response)
+print (snapshot_response[0]['name'])
 
-for snapshot in snapshot_response:
-    snapshot_name = snapshot[0]['diskSizeBytes']
-    print(snapshot_name)
+#for snapshot in snapshot_response:
+  #  snapshot_name = snapshot[0]['diskSizeBytes']
+ #   print(snapshot_name)
