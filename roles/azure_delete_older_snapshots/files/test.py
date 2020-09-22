baa3e86a8ebcda5ff6e5be979e09e2572786bc27
Aditya Patil <adpa@example.com>
2020-09-22 19:23:33 +0530

    Created test.py to pass az snapshot list result to test.py and Modified the task

---
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+import sys
+from datetime import datetime
+import pandas as pd
+
+snapshot_response = sys.argv[1]
+
+for snapshot in snapshot_response:
+    snapshot_name = snapshot['name']
+    print(snapshot_name)
