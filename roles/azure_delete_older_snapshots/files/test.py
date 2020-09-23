cbe10c4ebd141ef1effd2eed831e4667b95f1292
Aditya Patil <adpa@example.com>
2020-09-23 20:08:12 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -8,12 +8,12 @@ from datetime import datetime
 import pandas as pd
 
 #quotes = '"""'
-json_str_data = "'''"+ sys.argv[1] +"'''"
+json_str_data = sys.argv[1]
 json_data = json_str_data.replace("\'", "\"")
 #json_data = json.dumps(json_data)
 
 for snapshot in json_data:
-  snapshot_response = json.load(json_data, 'r')
+  snapshot_response = json.loads(json_data)
 #snapshot_response = json.dumps(snapshot_response)
 #snapshot_response = json.loads(snapshot_response)
   print(snapshot_response[0]["name"])
