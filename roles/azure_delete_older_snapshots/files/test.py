799da2bb932e92709624c13d0c4a47325c0b7374
Aditya Patil <adpa@example.com>
2020-09-23 20:49:45 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -13,7 +13,6 @@ for json_data in json_str_data:
   json_data = json_str_data.replace("\'", "\"")
 #json_data = json.dumps(json_data)
   print(json_data)
-
 #for snapshot in json_data:
  snapshot_response = json.loads(json_data)
 #snapshot_response = json.dumps(snapshot_response)
