64de46d418821459ddc522480609ab7f93d925f9
Aditya Patil <adpa@example.com>
2020-09-23 20:47:17 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -12,13 +12,13 @@ json_str_data = sys.argv[1]
 for json_data in json_str_data:
   json_data = json_str_data.replace("\'", "\"")
 #json_data = json.dumps(json_data)
-print(json_data)
+  print(json_data)
 
 #for snapshot in json_data:
- # snapshot_response = json.loads(json_data)
+ snapshot_response = json.loads(json_data)
 #snapshot_response = json.dumps(snapshot_response)
 #snapshot_response = json.loads(snapshot_response)
-  #print(snapshot_response[0]["name"])
+ print(snapshot_response[0]["name"])
 
 #for snapshot in snapshot_response:
   #  snapshot_name = snapshot[0]['diskSizeBytes']
