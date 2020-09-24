a5d4ec47b271fa7a0f2874b5bcab525e621531dc
Aditya Patil <adpa@example.com>
2020-09-24 12:50:58 +0530

    test

--- roles/azure_delete_older_snapshots/files/test.py
+++ roles/azure_delete_older_snapshots/files/test.py
@@ -19,7 +19,7 @@ for json_data in json_str_data:
   snapshot_response = json.loads(json_data, strict=False)
 #snapshot_response = json.dumps(snapshot_response)
 #snapshot_response = json.loads(snapshot_response)
-  print(json_data[3])
+  print(json_data[2])
 
 #for snapshot in snapshot_response:
   #  snapshot_name = snapshot[0]['diskSizeBytes']
