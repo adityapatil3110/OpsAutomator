c0fdab4d8cf897c155af7e1df019ba39e2b04dcc
Aditya Patil <adpa@example.com>
2020-09-30 00:26:35 +0530

    test to read and print the json objects from file

--- roles/azure_delete_older_snapshots/files/test1.py
+++ roles/azure_delete_older_snapshots/files/test1.py
@@ -12,4 +12,14 @@ json_str_data = sys.argv[1]
 
 f = open( '/home/ansible/Azure_Old_Snapshot_List.json', 'w' )
 f.write(json_str_data)
+
+
+f = open( '/home/ansible/Azure_Old_Snapshot_List.json', 'r' )
+
+snapshot_details = json.loads(f.read())
+
+for snapshot in snapshot_details:
+    print(snapshot)
+    
+    
 f.close()
