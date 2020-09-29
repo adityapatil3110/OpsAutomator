d1019be3e2831bf159e6e47501894a3bd30df08c
Aditya Patil <adpa@example.com>
2020-09-29 23:54:25 +0530

    test

--- roles/azure_delete_older_snapshots/files/test1.py
+++ roles/azure_delete_older_snapshots/files/test1.py
@@ -9,18 +9,7 @@ import pandas as pd
 
 #quotes = '"""'
 json_str_data = '"'+sys.argv[1]+'"'
-  #json_data = json_str_data.replace('\r\n', '')
-json_data = (json_str_data.replace('\n\t', ''))
-#json_data = json.dumps(json_data)
-  #json_data = json.dumps(json_str_data)
-print(json_data)
-#for snapshot in json_data:
-for json_data in json_str_data:
-  snapshot_response = json.loads(json_data, strict=False)
-#snapshot_response = json.dumps(snapshot_response)
-#snapshot_response = json.loads(snapshot_response)
-print(snapshot_response)
 
-#for snapshot in snapshot_response:
-  #  snapshot_name = snapshot[0]['diskSizeBytes']
- #   print(snapshot_name)
+f = open( '/home/ansible/Azure_Old_Snapshot_List.json', 'w' )
+f.write(dict)
+f.close()
