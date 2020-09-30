8142ec0d971137123ff1497ce72d10f8519aaaf8
Aditya Patil <adpa@example.com>
2020-10-01 00:30:18 +0530

    test

--- roles/test_azure_delete_snapshot/files/operate.py
+++ roles/test_azure_delete_snapshot/files/operate.py
@@ -7,8 +7,16 @@ import json
 from datetime import datetime
 import pandas as pd
 
+
+expiry_value = int()
+
 snapshot_report_file = sys.argv[1]
 
+######## Calculate difference of days between snapshot_creation_date and current_date
+def days_old(date):
+    date_obj = date.replace(tzinfo=None)
+    diff = datetime.now() - date_obj
+    return diff.days
 
 f = open( snapshot_report_file, 'r' )
 
@@ -19,13 +27,10 @@ snapshot_details = json.loads(f.read())
 for snapshot in snapshot_details:
     timeCreated_str = snapshot['timeCreated']
     timeCreated = datetime.strptime(timeCreated_str[:-6].replace("T", " "), '%Y-%m-%d %H:%M:%S.%f')
+    snapshot_age = days_old(timeCreated)
     snapshot_name = snapshot['name']
     location = snapshot['location']
     expiry_value = snapshot['tags']['Expiry']
     resource_group = snapshot['resourceGroup']
     
-print(timeCreated)
-print(snapshot_name)
-print(location)
-print(expiry_value)
-print(resource_group)
+print(snapshot_age)
