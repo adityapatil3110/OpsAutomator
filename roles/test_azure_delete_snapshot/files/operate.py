d1d52a3d68c3c01b994e8160d43c9f9d0d5855c9
Aditya Patil <adpa@example.com>
2020-10-04 19:20:31 +0530

    test

--- roles/test_azure_delete_snapshot/files/operate.py
+++ roles/test_azure_delete_snapshot/files/operate.py
@@ -41,14 +41,13 @@ for snapshot in snapshot_details:
     #print(snapshot_name)
 
     if snapshot_age > limit:
-        for snapshot in snapshot_details:
-            snap_list.append(snapshot['name'])   
-            start_time = str(timeCreated)
-            start_time_list.append(start_time)
-            snapshot_age_list.append(snapshot_age)
+        #for snapshot in snapshot_details:
+        snap_list.append(snapshot['name'])   
+        start_time = str(timeCreated)
+        start_time_list.append(start_time)
+        snapshot_age_list.append(snapshot_age)
             
         dict = {'SnapshotNames':snap_list, 'StartTime':start_time_list, 'SnapshotAge':snapshot_age_list, 'Expiry_Limit_in_Days':tag_dict}
-        print(snap_list)
         # Generate the Report name for deleted snapshots and push the column names and details of the deleted snapshots
         now = datetime.now()
         date_time = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -57,3 +56,5 @@ for snapshot in snapshot_details:
         #print (filename)
         df = pd.DataFrame(dict)
         df.to_csv(filename, index=False) 
+        
+print(snap_list)
