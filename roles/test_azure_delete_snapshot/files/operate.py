21f392dbe53674588d1cce4899870eccfa59d924
Aditya Patil <adpa@example.com>
2020-10-07 16:24:02 +0530

    test

--- roles/test_azure_delete_snapshot/files/operate.py
+++ roles/test_azure_delete_snapshot/files/operate.py
@@ -72,6 +72,7 @@ for snapshot in snapshot_details:
         #SENDER = sys.argv[2]
         #RECIPIENT = sys.argv[3]
         
+        SENDGRID_API_KEY='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'
                 
         message = Mail(
             from_email='patiladi3110@gmail.com',
@@ -91,7 +92,7 @@ for snapshot in snapshot_details:
         attachment.content_id = ContentId('Snapshot Deletion')
         message.attachment = attachment
         try:
-            sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            sendgrid_client = SendGridAPIClient(os.environ.get(SENDGRID_API_KEY))
             response = sendgrid_client.send(message)
             #print(response.status_code)
             #print(response.body)
